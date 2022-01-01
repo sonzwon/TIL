d22a315a41eb148540ca3c88bbab42fd5bd38b83
jiwon <sby04056@gmail.com>
2022-01-02 02:27:02 +0900

    TIL220102

--- python_practice.py
+++ python_practice.py
@@ -112,5 +112,38 @@ print(string.replace('a','A'))
 # 'Abcdfe2A354A32A'
 string = 'abcd'
 print(string.replace('b', 'B'))
-# aBcd
+# aBcda = "3"
+b = "4"
+print(a+b)
+# 34
+print("Hi"*3)
+# HiHiHi
+t1 = 'python'
+t2 = 'java'
+t3 = t1 + ' ' + t2 + ' '
+print(t3*4)
+# python java python java python java python java 
+name1 = "김민수"
+age1 = 10
+name2 = "이철희"
+age2 = 13
+print('이름 : %s 나이 : %d' %(name1, age1))
+print('이름 : %s 나이 : %d' %(name2, age2))
+print('이름 : {} 나이 : {}' .format(name1, age1))
+print('이름 : {} 나이 : {}' .format(name2, age2))
+print(f"이름: {name1} 나이: {age1}")
+print(f"이름: {name2} 나이: {age2}")
+# 이름 : 김민수 나이 : 10
+# 이름 : 이철희 나이 : 13
+상장주식수 = "5,969,782,550"
+a = 상장주식수.replace(',','')
+A = int(a)
+type(A)
+# int
+분기 = "2020/03(E) (IFRS연결)"
+print(분기[:7])
+# 2020/03
+data = "   삼성전자    "
+print(data.strip())
+# 삼성전자
 
