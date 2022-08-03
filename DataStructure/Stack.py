75f162d003efb22ff28d44ba36c46a75602a77ce
sonzwon <sby04056@gmail.com>
2022-08-03 19:45:16 +0900

    20220803

--- DataStructure/Stack.py
+++ DataStructure/Stack.py
@@ -12,14 +12,12 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
-    def isEmpty(self):
-        if self.head.get_next()==None:
-            return True
+
 
 class Stack:
     def __init__(self, head=None):
         self.size = 0
-        self.head = head
+        self.head = Node()
     
     def push(self, data):
         new_node = Node(data, self.head.get_next())
@@ -27,16 +25,16 @@ class Stack:
         self.size += 1
     
     def pop(self):
+        if self.size == 0:
+            return None
         curr = self.head.get_next() #헤드의 넥스트는 항상 최신 노드이므로
         self.head.set_next(curr.get_next())
         curr.set_next(None)
         self.size -= 1
-        if isEmpty == True:
-            return None
         return curr.get_data()
     
     def peak(self):
-        if isEmpty == True:
+        if self.size == 0:
             return None
         return self.head.get_next().get_data()
     
