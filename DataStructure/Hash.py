2a8a79f13f66039579746516858a92d28bc5ef35
sonzwon <sby04056@gmail.com>
2022-08-13 22:55:27 +0900

    hash

--- DataStructure/Hash.py
+++ DataStructure/Hash.py
@@ -5,7 +5,7 @@ class Node:
 
 class HashTable:
     def __init__(self, bucketsize=1024):
-        self.buckets = [[]]*bucketsize
+        self.buckets = [[]] * bucketsize
         self.size = 0
         self.bucketsize = bucketsize
         
@@ -18,3 +18,27 @@ class HashTable:
         node = Node(key, val)
         self.buckets[idx].append(node)
         self.size+=1
+        
+    def get(self, key):
+        idx = hash(key)% self.bucketsize
+        for element in self.buckets[idx]:
+            if element.key == key:
+                return element.value
+        return None
+    
+    def delete(self, key):
+        idx = hash(key)% self.bucketsize
+        for idx, element in enumerate(self.buckets[idx]):
+            if element.key == key:
+                self.buckets[idx].remove(element)
+                self.size -= 1
+    
+    def contains(self, key):
+        idx = hash(key)% self.bucketsize
+        for element in self.buckets[idx]:
+            if element.key == key:
+                return True
+        return False
+        
+    def size(self):
+        return self.size
