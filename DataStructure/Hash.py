925d3819ef4577efbc70a85dd106734fcdfe976e
sonzwon <sby04056@gmail.com>
2022-08-13 23:25:25 +0900

    hash

--- DataStructure/Hash.py
+++ DataStructure/Hash.py
@@ -14,8 +14,7 @@ class HashTable:
         for element in self.buckets[idx]:  ##key는 고유한 값이여야하기 떄문에
             if element.key == key:
                 element.value == value
-                return
-        node = Node(key, val)
+        node = Node(key, value)
         self.buckets[idx].append(node)
         self.size+=1
         
@@ -42,3 +41,31 @@ class HashTable:
         
     def size(self):
         return self.size
+    
+    
+    
+## Test
+if __name__ == "__main__":
+    table = HashTable()
+    print("table.put('k1','v1')")
+    table.put('k1','v1')
+    print("table.put('k2','v2')")
+    table.put('k2','v2')
+    print("table.put('k3','v3')")
+    table.put('k3','v3')
+    print("table.get('k1') : {}".format(table.get('k1')))
+    print("table.get('k2') : {}".format(table.get('k2')))
+    print("table.get('k3') : {}".format(table.get('k3')))
+    print("table.put('k2','v4')")
+    table.put('k2','v4')
+    print("table.get('k2') : {}".format(table.get('k2')))
+    print("table.delete('k2')")
+    table.delete('k2')
+    print("table.get('k1') : {}".format(table.get('k1')))
+    print("table.get('k2') : {}".format(table.get('k2')))
+    print("table.get('k3') : {}".format(table.get('k3')))
+
+
+# 해결해야할 오류
+# 1) put메서드 key값 충돌일어났을 때, 덮어쓰기 기능 안됨
+# 2) table.size 실행시 TypeError: 'int' object is not callable 발생
