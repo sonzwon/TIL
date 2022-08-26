c53d68a9f5d85327eaeb7e46a1f5b5ef8cbab090
sonzwon <sby04056@gmail.com>
2022-08-26 14:52:35 +0900

    topological sort

--- DataStructure/Graph.py
+++ DataStructure/Graph.py
@@ -42,3 +42,50 @@ if __name__=='__main__':
     
     print(f"BFS(graph,0): {BFS(graph,0)}")
     print(f"DFS(graph,0): {DFS(graph,0)}")
+
+
+
+
+# [ Topological Sort ]
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.vertexes = set()
+    def add(self, start, end, weight):
+        self.vertexes.add(start)
+        self.vertexes.add(end)
+        self.graph[start].append((end, weight))
+    def getVertexes(self):
+        return self.vertexes
+    def getNodes(self, vertex):
+        return self.graph[vertex]
+        
+
+
+# 1. queue를 이용한 구현
+def TopologicalSortQ(graph):
+
+
+
+
+# 2. stack을 이용한 구현
+def TopologicalSort(graph):
+    def TopologicalSort_(graph, vertex, visited, stack):
+        visited.add(vertex)
+        nodes = graph.getNodes(vertex)
+        for n in nodes:
+            if n not in visited:
+                TopologicalSort_(graph, n, visited, stack)
+        stack.append(vertex)
+        
+    result = []
+    stack = []
+    visited = set()
+    
+    vertexes = graph.getVertexes()
+    for v in vertexes:
+        if vertex not in visited:
+            TopologicalSort_(graph, vertex, visited, stack)
+    while stack:
+        result.append(stack.pop())
+    return result
